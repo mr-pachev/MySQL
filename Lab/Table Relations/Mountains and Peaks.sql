8226f9ce3b8d993f9578f4fe9224f653aeb6247f
Ivan Pachev <mr.pachev@abv.bg>
2024-01-24 14:29:54 +0200

    Create Mountains and Peaks.sql

---
+++ Lab/Table Relations/Mountains and Peaks.sql
@@ -0,0 +1,13 @@
+CREATE TABLE mountains(
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE peaks(
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(50),
+    mountain_id INT NOT NULL,
+		CONSTRAINT fk_peaks_mountain_id_mountains_id
+			FOREIGN KEY (mountain_id)
+            REFERENCES mountains(id)
+);
