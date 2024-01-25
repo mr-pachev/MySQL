331b58edda0bd233e25e93761f4edeafa6261a93
Ivan Pachev <mr.pachev@abv.bg>
2024-01-25 21:12:31 +0200

    Create Peaks in Rila.sql

---
+++ Exercises/Exercise Table Relations/Peaks in Rila.sql
@@ -0,0 +1,8 @@
+SELECT 
+	mountain_range, 
+	peak_name,
+    elevation AS 'peak_elevation'
+FROM peaks
+JOIN mountains ON peaks.mountain_id = mountains.id
+WHERE mountains.mountain_range = 'Rila'
+ORDER BY `peak_elevation` DESC;
