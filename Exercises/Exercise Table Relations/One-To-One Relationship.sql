587483665c88f9f358faafb4d7f8c6f85bde31e6
Ivan Pachev <mr.pachev@abv.bg>
2024-01-25 18:42:52 +0200

    Update One-To-One Relationship.sql

---
+++ Exercises/Exercise Table Relations/One-To-One Relationship.sql
@@ -0,0 +1,24 @@
+CREATE TABLE passports(
+	passport_id INT PRIMARY KEY AUTO_INCREMENT,
+    passport_number VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE people(
+	person_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    salary DECIMAL (9, 2),
+    passport_id INT NOT NULL UNIQUE,
+    CONSTRAINT fk
+    FOREIGN KEY (passport_id)
+    REFERENCES passports(passport_id)
+);
+
+INSERT INTO passports (passport_id, passport_number) VALUES 
+('101', 'N34FG21B'),
+('102', 'K65LO4R7'),
+('103', 'ZE657QP2');
+
+INSERT INTO people (first_name, salary, passport_id) VALUES 
+('Roberto', '43300.00', '102'),
+('Tom', '56100.00', '103'),
+('Yana', '60200.00', '101');
