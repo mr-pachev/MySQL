a8433fb95fc753e94c7e26a31b93a45f7e228d25
Ivan Pachev <mr.pachev@abv.bg>
2024-01-27 16:21:36 +0200

    Create Employees with Project.sql

---
+++ Exercises/Exercise Subqueries and JOINs/Employees with Project.sql
@@ -0,0 +1,10 @@
+SELECT		
+	e.employee_id,
+    e.first_name,
+    p.name AS 'project_name'
+FROM employees AS e
+JOIN employees_projects AS ep ON e.employee_id = ep.employee_id
+JOIN projects AS p ON ep.project_id = p.project_id
+WHERE e.hire_date > '200-08-13' AND p.end_date IS NULL
+ORDER BY e.first_name, `project_name`
+LIMIT 5;
