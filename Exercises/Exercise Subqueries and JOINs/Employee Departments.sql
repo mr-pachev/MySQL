a56692cbe11f156a11493c2441ace50c63e9fea8
Ivan Pachev <mr.pachev@abv.bg>
2024-01-27 15:32:04 +0200

    Update Employee Departments.sql

--- Exercises/Exercise Subqueries and JOINs/Employee Departments.sql
+++ Exercises/Exercise Subqueries and JOINs/Employee Departments.sql
@@ -1,7 +1,7 @@
 SELECT
 	employee_id,
     first_name,
-    last_name,
+    e.salary,
     d.name AS 'department_name'
 FROM employees AS e
 JOIN departments AS d ON e.department_id = d.department_id
