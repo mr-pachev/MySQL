cd4e133d6989d563a6350aef07aa5522d855155c
Ivan Pachev <mr.pachev@abv.bg>
2024-01-27 19:08:17 +0200

    Update Count Mountain Ranges.sql

--- Exercises/Exercise Subqueries and JOINs/Count Mountain Ranges.sql
+++ Exercises/Exercise Subqueries and JOINs/Count Mountain Ranges.sql
@@ -3,6 +3,6 @@ SELECT
     COUNT(m.mountain_range) AS 'mountain_range'
 FROM mountains_countries AS mc
 JOIN mountains AS m ON m.id = mc.mountain_id
+WHERE mc.country_code IN ('US', 'RU', 'BG')
 GROUP BY geography.mc.country_code
-HAVING mc.country_code IN ('US', 'RU', 'BG')
 ORDER BY `mountain_range` DESC;
