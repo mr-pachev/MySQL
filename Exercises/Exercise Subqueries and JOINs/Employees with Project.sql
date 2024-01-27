def8a337bf0cd3610ac58be78cd07bbedc786049
Ivan Pachev <mr.pachev@abv.bg>
2024-01-27 16:33:32 +0200

    Update Employees with Project.sql

--- Exercises/Exercise Subqueries and JOINs/Employees with Project.sql
+++ Exercises/Exercise Subqueries and JOINs/Employees with Project.sql
@@ -5,6 +5,6 @@ SELECT
 FROM employees AS e
 JOIN employees_projects AS ep ON e.employee_id = ep.employee_id
 JOIN projects AS p ON ep.project_id = p.project_id
-WHERE DATE(e.hire_date) > '200-08-13' AND p.end_date IS NULL
-ORDER BY e.first_name, `project_name`
+WHERE DATE(p.start_date) > '2002-08-13' AND p.end_date IS NULL
+ORDER BY e.first_name, p.name
 LIMIT 5;
