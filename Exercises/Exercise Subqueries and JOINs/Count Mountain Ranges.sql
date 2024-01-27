3367cdadb3aca8f95d5a1a21dbf03dea593d5a8d
Ivan Pachev <mr.pachev@abv.bg>
2024-01-27 19:07:00 +0200

    Create Count Mountain Ranges.sql

---
+++ Exercises/Exercise Subqueries and JOINs/Count Mountain Ranges.sql
@@ -0,0 +1,8 @@
+SELECT
+	mc.country_code,
+    COUNT(m.mountain_range) AS 'mountain_range'
+FROM mountains_countries AS mc
+JOIN mountains AS m ON m.id = mc.mountain_id
+GROUP BY geography.mc.country_code
+HAVING mc.country_code IN ('US', 'RU', 'BG')
+ORDER BY `mountain_range` DESC;
