1086985df8a2cfd6551e9880933dbc7f9941fd99
Ivan Pachev <mr.pachev@abv.bg>
2024-01-27 16:13:24 +0200

    Create Employees Hired After.sql

---
+++ Exercises/Exercise Subqueries and JOINs/Employees Hired After.sql
@@ -0,0 +1,9 @@
+SELECT
+	first_name,
+    last_name,
+    hire_date,
+    d.name AS 'dept_name'
+FROM employees AS e
+JOIN departments AS d ON e.department_id = d.department_id
+WHERE e.hire_date > '1999-1-1' AND d.name IN ('Sales', 'Finance')
+ORDER BY e.hire_date;
