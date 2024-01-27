e17140f0d49889d24b160bdbc201f9519aeb81e1
Ivan Pachev <mr.pachev@abv.bg>
2024-01-27 15:32:46 +0200

    Update Employee Departments.sql

--- Exercises/Exercise Subqueries and JOINs/Employee Departments.sql
+++ Exercises/Exercise Subqueries and JOINs/Employee Departments.sql
@@ -6,4 +6,5 @@ SELECT
 FROM employees AS e
 JOIN departments AS d ON e.department_id = d.department_id
 WHERE e.salary > 15000
-ORDER BY e.department_id DESC;
+ORDER BY e.department_id DESC
+LIMIT 5;
