899717c0cd807840391747fcad66171a0d72608f
Ivan Pachev <mr.pachev@abv.bg>
2024-01-29 10:22:33 +0200

    Update Private flights.sql

--- Exam Prep/Database Basics MySQL Retake Exam - 11 December 2022/Private flights.sql
+++ Exam Prep/Database Basics MySQL Retake Exam - 11 December 2022/Private flights.sql
@@ -1,10 +1,7 @@
 SELECT
-	UPPER(CONCAT(LEFT(p.last_name, 2), p.country_id)) AS 'flight_code',
-    CONCAT(p.first_name, ' ', p.last_name) AS 'full_name',
-    p.country_id
-FROM passengers AS p
-LEFT JOIN flights_passengers AS fp ON p.id = fp.passenger_id
-LEFT JOIN flights AS f ON fp.flight_id = f.id
-LEFT JOIN countries AS c ON f.destination_country = c.id
-WHERE fp.flight_id IS NULL
-ORDER BY p.country_id;
+	UPPER(CONCAT(LEFT(last_name, 2), country_id)) AS 'flight_code',
+    CONCAT(first_name, ' ', last_name) AS 'full_name',
+    country_id
+FROM passengers
+WHERE id NOT IN(SELECT passenger_id FROM flights_passengers)
+ORDER BY country_id;
