7eab9d6c8e9c0448a974b478fb4186998057c3ce
Ivan Pachev <mr.pachev@abv.bg>
2024-02-01 10:27:56 +0200

    Create Employees with Salary Above Number.sql

---
+++ Exercises/Exercise Functions and Procedures/Employees with Salary Above Number.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE usp_get_employees_salary_above(given_number DECIMAL (19.4))
+BEGIN
+	SELECT first_name, last_name
+    FROM employees
+    WHERE salary >= given_number
+    ORDER BY first_name, last_name, employee_id;
+END
