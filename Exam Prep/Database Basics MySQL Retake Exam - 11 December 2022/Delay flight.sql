33f5d61629d24fcbdf098dba077149eefa5e7fbc
Ivan Pachev <mr.pachev@abv.bg>
2024-02-01 20:48:19 +0200

    Create Delay flight.sql

---
+++ Exam Prep/Database Basics MySQL Retake Exam - 11 December 2022/Delay flight.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE udp_delay_flight(code VARCHAR(50))
+BEGIN 
+	UPDATE flights
+    SET has_delay = 1
+    WHERE flight_code = code;
+    UPDATE flights
+    SET departure = DATE_ADD(departure, INTERVAL 30 MINUTE)
+    WHERE flight_code = code;
+END$$
