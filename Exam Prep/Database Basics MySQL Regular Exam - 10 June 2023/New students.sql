0da498d1a5f09aac16fa832aef949e856ec1ca8a
Ivan Pachev <mr.pachev@abv.bg>
2024-02-03 16:15:47 +0200

    Create New students.sql

---
+++ Exam Prep/Database Basics MySQL Regular Exam - 10 June 2023/New students.sql
@@ -0,0 +1,8 @@
+SELECT
+	CONCAT(first_name, ' ', last_name) AS 'full_name',
+    SUBSTRING(email, 2, 10) AS 'username',
+    REVERSE(phone) AS 'password'
+FROM students AS s
+LEFT JOIN students_courses AS sc ON s.id = sc.student_id
+WHERE sc.course_id IS NULL
+ORDER BY `password` DESC;
