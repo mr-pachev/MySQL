8d6f019f8b3cdf9559a6472578323acd9a686ab2
Ivan Pachev <mr.pachev@abv.bg>
2024-02-04 15:10:09 +0200

    Create Best categories.sql

---
+++ Exam Prep/Database Basics MySQL Regular Exam - 13 February 2022/Best categories.sql
@@ -0,0 +1,9 @@
+SELECT
+	COUNT(p.category_id) AS 'items_count',
+    c.name,
+   SUM(p.quantity_in_stock) AS 'total_quantity'
+FROM categories AS c
+JOIN products AS p ON c.id = p.category_id
+GROUP BY c.name
+ORDER BY `items_count` DESC, `total_quantity`
+LIMIT 5;
