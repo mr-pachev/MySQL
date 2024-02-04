291282d10e7bca30a5ede18eee936efdf9c0aa25
Ivan Pachev <mr.pachev@abv.bg>
2024-02-04 13:12:25 +0200

    Create Insert.sql

---
+++ Exam Prep/Database Basics MySQL Regular Exam - 13 February 2022/Insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO reviews (content, picture_url, published_at, rating)
+SELECT
+	SUBSTRING(description, 1, 15),
+    REVERSE(name),
+    '2010-10-10',
+    (price / 8)
+FROM products
+WHERE id >= 5;
