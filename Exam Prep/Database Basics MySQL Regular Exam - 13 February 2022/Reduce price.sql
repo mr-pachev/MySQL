e6a9a594ed3610cee7e2a92a2ffca65d2e30b418
Ivan Pachev <mr.pachev@abv.bg>
2024-02-04 16:38:45 +0200

    Update Reduce price.sql

--- Exam Prep/Database Basics MySQL Regular Exam - 13 February 2022/Reduce price.sql
+++ Exam Prep/Database Basics MySQL Regular Exam - 13 February 2022/Reduce price.sql
@@ -4,5 +4,6 @@ UPDATE products AS p
 JOIN reviews AS r ON p.review_id = r.id
 JOIN categories AS c ON p.category_id = c.id
 SET price = ROUND(price * 0.7, 2)
+-- SET p.price = ROUND(p.price - (p.price * 30 / 100), 2) 
 WHERE r.rating < 4 AND category_name = c.name;
 END$$
