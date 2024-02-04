7b11fe9dd93f6aea91eb56aa5828365b9bfcb986
Ivan Pachev <mr.pachev@abv.bg>
2024-02-04 20:41:26 +0200

    Update Games of 2022.sql

--- Exam Prep/Database Basics MySQL Retake Exam - 6 August 2021/Games of 2022.sql
+++ Exam Prep/Database Basics MySQL Retake Exam - 6 August 2021/Games of 2022.sql
@@ -8,7 +8,9 @@ WHEN MONTH(release_date) IN (04, 05, 06) THEN 'Q2'
 WHEN MONTH(release_date) IN (07, 08, 09) THEN 'Q3'
 WHEN MONTH(release_date) IN (10, 11, 12) THEN 'Q4'
 END) AS 'quarter',
-(SELECT name FROM teams WHERE id = g.team_id)   AS 'team_name'
+(SELECT name FROM teams WHERE id = g.team_id) AS 'team_name'
 FROM games AS g
-WHERE name LIKE '%2' AND MONTH(release_date) % 2 = 0
+WHERE g.name LIKE '%2' AND 
+	MONTH(g.release_date) % 2 = 0 AND 
+	YEAR(g.release_date) = 2022
 ORDER BY `quarter`;
