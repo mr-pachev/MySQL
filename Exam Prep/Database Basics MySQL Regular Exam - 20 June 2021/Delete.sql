e3dc7779c0a9fbfb7e724c11c3a163cb07328d19
Ivan Pachev <mr.pachev@abv.bg>
2024-02-05 15:08:40 +0200

    Update Delete.sql

--- Exam Prep/Database Basics MySQL Regular Exam - 20 June 2021/Delete.sql
+++ Exam Prep/Database Basics MySQL Regular Exam - 20 June 2021/Delete.sql
@@ -1,3 +1,3 @@
 DELETE c FROM clients AS c
 LEFT JOIN courses AS cor ON c.id = cor.client_id
-WHERE c.id IS NULL AND LENGTH(c.full_name) > 3;
+WHERE cor.client_id IS NULL AND LENGTH(c.full_name) > 3;
