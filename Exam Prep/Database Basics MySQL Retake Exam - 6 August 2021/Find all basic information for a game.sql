4abaa9fb33a41ec44379396f4d3013905f1d92b0
Ivan Pachev <mr.pachev@abv.bg>
2024-02-05 10:32:59 +0200

    Create Find all basic information for a game.sql

---
+++ Exam Prep/Database Basics MySQL Retake Exam - 6 August 2021/Find all basic information for a game.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+CREATE FUNCTION udf_game_info_by_name (game_name VARCHAR (20))
+RETURNS TEXT
+DETERMINISTIC
+BEGIN
+	DECLARE info TEXT;
+    SET info := (SELECT 
+				CONCAT('The ', g.name, ' is developed by a ', t.name, ' in an office with an address ', a.name) 
+                FROM games AS g
+                JOIN teams AS t ON g.team_id = t.id
+                JOIN offices AS o ON t.office_id = o.id
+                JOIN addresses AS a ON o.address_id = a.id
+                WHERE g.name = game_name);
+    RETURN info;
+END$$
