243a7787e82b8322e5589a0793abd4729778d346
Ivan Pachev <mr.pachev@abv.bg>
2024-02-05 15:35:15 +0200

    Update

--- Exam Prep/Database Basics MySQL Regular Exam - 20 June 2021/Number of courses.sql
+++ Exam Prep/Database Basics MySQL Regular Exam - 20 June 2021/Number of courses.sql
@@ -5,7 +5,7 @@ SELECT
 	COUNT(cor.from_address_id) AS 'count_of_courses',
 	ROUND(AVG(cor.bill), 2) AS 'avg_bill'
 FROM cars AS c
-JOIN courses AS cor ON c.id = cor.car_id
+LEFT JOIN courses AS cor ON c.id = cor.car_id
 GROUP BY c.id
 HAVING `count_of_courses` <> 2
 ORDER BY `count_of_courses` DESC, c.id;
