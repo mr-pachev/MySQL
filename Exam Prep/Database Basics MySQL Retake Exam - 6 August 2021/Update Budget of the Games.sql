c460dc367078cfbafedfa87f9a9dc25f89100120
Ivan Pachev <mr.pachev@abv.bg>
2024-02-05 10:57:01 +0200

    Update Update Budget of the Games.sql

--- Exam Prep/Database Basics MySQL Retake Exam - 6 August 2021/Update Budget of the Games.sql
+++ Exam Prep/Database Basics MySQL Retake Exam - 6 August 2021/Update Budget of the Games.sql
@@ -4,10 +4,16 @@ BEGIN
     LEFT JOIN games_categories AS gc ON g.id = gc.game_id
     LEFT JOIN categories AS c ON gc.category_id = c.id
     SET g.budget = g.budget + 100000
-    WHERE c.id IS NULL AND g.rating < min_game_rating;
+    WHERE c.id IS NULL AND 
+		g.rating > min_game_rating AND 
+        min_game_rating > 0 AND 
+        g.release_date IS NOT NULL;
     UPDATE games AS g
     LEFT JOIN games_categories AS gc ON g.id = gc.game_id
     LEFT JOIN categories AS c ON gc.category_id = c.id
     SET g.release_date = DATE_ADD(g.release_date, INTERVAL 1 YEAR)
-    WHERE c.id IS NULL AND g.rating < min_game_rating;
+    WHERE c.id IS NULL AND 
+    g.rating > min_game_rating AND 
+    min_game_rating > 0 AND 
+    g.release_date IS NOT NULL;
 END$$
