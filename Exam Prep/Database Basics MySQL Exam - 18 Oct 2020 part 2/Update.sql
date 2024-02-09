ee220bb29008808d759bc5308f05cd7a42cdeb0a
Ivan Pachev <mr.pachev@abv.bg>
2024-02-09 13:13:16 +0200

    Update Update.sql

--- Exam Prep/Database Basics MySQL Exam - 18 Oct 2020 part 2/Update.sql
+++ Exam Prep/Database Basics MySQL Exam - 18 Oct 2020 part 2/Update.sql
@@ -1,4 +1,4 @@
 UPDATE employees
 SET salary = salary - 500,
 	manager_id = 3
-WHERE YEARH(hire_date) > 2003 AND store_id <> store_id IN (5, 14);
+WHERE YEAR(hire_date) > 2003 AND store_id <> 5 AND store_id <> 14;
